ad97972bf35a194b3584322abcb35f32891fb992
Shiv_Boy <shivamchouhan728@gmail.com>
2021-01-13 21:46:50 +0530

    django api setup

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-# from .views import PostList, PostDetail
+from .views import PostList, PostDetail
 
-# app_name = "blog_api"
+app_name = "blog_api"
 
-# urlpatterns = [
-#     path('<int:pk>/', PostDetail.as_view(), name="detail-create"),
-#     path('', PostList.as_view(), name="list-create")
-# ]
+urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name="detail-create"),
+    path('', PostList.as_view(), name="list-create")
+]
