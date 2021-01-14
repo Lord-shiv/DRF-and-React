5d280effa902e8b59412be0b62d22659d733fc7f
Shiv_Boy <shivamchouhan728@gmail.com>
2021-01-14 13:39:26 +0530

    Added Api GET/PUT/Delete/Update

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -4,6 +4,6 @@ from .views import PostList, PostDetail
 app_name = "blog_api"
 
 urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view(), name="detail-create"),
-    path('', PostList.as_view(), name="list-create")
+    path('<int:pk>/', PostDetail.as_view(), name="detailcreate"),
+    path('', PostList.as_view(), name="listcreate")
 ]
